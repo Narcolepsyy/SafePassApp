ac866d1d41c65b4be48a6c8d1d210bf5c9c3d446
khaitran <23520680@gm.uit.edu.vn>
2025-08-20 02:00:09 +0700

    add source

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,6 +5,18 @@ class EntriesController < ApplicationController
     @entry = Entry.new
   end
   def create
-    @entry = current_user.entries.new
+    @entry = current_user.entries.new(entry_params)
+
+    if @entry.save
+      flash[:notice] = "Entry has been created."
+      redirect_to root_path
+    else
+      flash[:alert] = "Sorry, there was an error."
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  def entry_params
+    params.expect(entry: [ :name, :url, :username, :password ])
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,14 @@
 class Entry < ApplicationRecord
   belongs_to :user
+
+  validates :name, :username, :password, presence: true
+  validate :url_must_be_valid
+
+  private
+
+  def url_must_be_valid
+    unless url.to_s.start_with?('http://', 'https://')
+      errors.add(:url, "must start with http:// or https://")
+    end
+  end
 end
